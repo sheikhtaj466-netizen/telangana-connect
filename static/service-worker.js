f5abd959a8df05e6c354306ebf81432cb061d3ab
Sheikh Taj <sheikhtaj466@gmail.com>
2025-10-14 11:43:41 +0530

    Updated service worker for robust PWA

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,9 +1,50 @@
-// Basic Service Worker for PWA installation
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
+const CACHE_NAME = 'tg-connect-cache-v1';
+const urlsToCache = [
+  '/',
+  '/?theme=aurora',
+  '/static/style.css',
+  '/static/script.js',
+  '/static/manifest.json'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-  // This is a placeholder. We'll add caching later.
-  event.respondWith(fetch(event.request));
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
